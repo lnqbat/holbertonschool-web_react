6d55e5945dfe3da70b28e3585cc9615eae374ea8
lnqbat <b.lonqueu98@gmail.com>
2025-09-11 16:05:01 +0200

    Add teachClass function to handle subject teaching logic

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -61,5 +61,13 @@ function executeWork(employee: any): string {
   }
 }
 
-console.log(executeWork(createEmployee(200)));
-console.log(executeWork(createEmployee(1000)));
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  }
+  else {
+    return "Teaching History";
+  }
+}
